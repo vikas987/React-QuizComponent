d55ec619f5db5de80d7a19ca0f576ea186243e93
Vikas Shivaraj <M1046867@mindtree.com>
2021-01-24 21:48:20 +0530

    changes

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -19,12 +19,10 @@ class QuizQuestion extends Component {
                 <section className="buttons">
                     <ul>
                         {
-                            this.props.quiz_question.answer_options
-                                .map((answer_option, index) =>
-                                    <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />
-                                )
+                            this.props.quiz_question.answer_options.map((answer_option, index) => {
+                                return <QuizQuestionButton key={index} button_text={answer_option} clickHandler={this.handleClick.bind(this)} />
+                            })
                         }
-
                     </ul>
                 </section>
             </main>
