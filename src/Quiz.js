dc75072924ca3303394ddc0c7e601166e274d40d
Vikas Shivaraj <M1046867@mindtree.com>
2021-01-24 22:00:31 +0530

    changes

--- src/Quiz.js
+++ src/Quiz.js
@@ -16,13 +16,17 @@ class Quiz extends Component {
         this.setState(prevState => ({ quiz_position: prevState.quiz_position + 1 }))
     }
 
+    handleResetClick() {
+        this.setState({ quiz_position: 1 })
+    }
+
     render() {
         const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length
         return (
             <div>
                 {
                     isQuizEnd ?
-                        <QuizEnd /> :
+                        <QuizEnd resetClickHandler={this.handleResetClick.bind(this)} /> :
                         <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)} />
                 }
             </div>
